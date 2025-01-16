dfc4d8fd69a248f3235b48b907de3028aff141d2
José Vilaça <jose.vilaca@xpand-it.com>
2025-01-16 18:10:29 +0000

    stg, marts, ..., tests, snapshots

---
+++ models/marts/core/fct_orders.sql
@@ -0,0 +1,22 @@
+with 
+
+orders as  (
+    select * from {{ ref ('stg_orders' )}}
+),
+
+payments as (
+    select * from {{ ref ('stg_payment') }}
+),
+
+fct_orders as (
+
+    select
+        order_id,
+        payment_id,
+        amount
+
+    from orders o
+    left join payments p using (order_id)
+)
+
+select * from fct_orders
